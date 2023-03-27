44269f416dde2ad5f4e56a2f3162316186fd1db4
vladtanasiedns <tanasie.vlad@dandsltd.com>
2023-03-27 10:10:08 +0300

    Added components and metatags

---
+++ src/components/HeroSection.tsx
@@ -0,0 +1,9 @@
+export const HeroSection = () => {
+    return <> 
+        <article>
+            <section>
+                <h1>Hero Section</h1>
+            </section>
+       </article> 
+   </>; 
+};
---
+++ src/components/Navbar.tsx
@@ -0,0 +1,14 @@
+export const Navbar = () => {
+    return <>
+        <nav>
+            <ul>
+                <li>Home</li>
+                <li>About</li>
+                <li>Menu</li>
+                <li>Reservations</li>
+                <li>Order Online</li>
+                <li>Login</li>
+            </ul>
+        </nav>
+    </> 
+}
